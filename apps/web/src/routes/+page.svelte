d36d33c10b53242f52af81199aab00d90e0e08ac
Alejandro Alonso Mayo <alejandroalonsomayo@gmail.com>
2024-09-05 23:27:51 +0200

    feat: :sparkles: add basic header and theme

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,1 +1,31 @@
-<h1 class="text-primary text-4xl font-bold">FreeHomeRP</h1>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum congue felis nec sodales. Donec quis sapien
+mollis massa volutpat egestas a eu mi. Pellentesque in pulvinar est, eget placerat metus. Aliquam aliquet leo et eros
+finibus, et suscipit dolor pellentesque. Ut tristique mi ac lectus congue, id suscipit justo porta. Fusce velit urna,
+vulputate ut velit at, suscipit commodo velit. Phasellus nec pharetra diam. Morbi nec sem et nulla lacinia porta ac quis
+metus. Nunc non euismod odio. Suspendisse felis massa, faucibus eu fringilla in, facilisis venenatis velit. Integer
+bibendum, turpis in ultrices finibus, felis risus cursus metus, in pharetra ligula orci ac tellus. Suspendisse tincidunt
+aliquet nulla id consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut
+convallis, leo vitae porttitor vehicula, dui metus volutpat velit, eget gravida eros est vitae justo. Maecenas tempor
+quam at placerat mattis. Etiam enim velit, imperdiet ut rutrum ac, auctor at sapien. Praesent placerat velit nulla, nec
+elementum dui elementum eget. Vestibulum imperdiet cursus condimentum. Vestibulum scelerisque posuere justo, consequat
+laoreet lectus vehicula et. Aenean vel pharetra massa, et commodo ipsum. In hac habitasse platea dictumst. Donec
+fringilla sem ut magna dictum sagittis. Nam ac massa finibus, aliquam nisi sed, faucibus urna. In tempus, purus nec
+tempus facilisis, leo sem euismod elit, nec dapibus tortor nisi pretium leo. Etiam fermentum vitae elit ac luctus.
+Praesent finibus nisl justo, ac placerat eros scelerisque a. Curabitur consequat mattis enim in accumsan. Suspendisse
+lobortis efficitur nisi at consequat. Donec ornare est mauris, ac tincidunt dolor rhoncus sed. Etiam pulvinar quam quis
+nisl ultrices, at efficitur erat tempus. Nunc finibus vulputate odio, sed suscipit quam accumsan eu. Aenean vestibulum,
+dolor et dapibus hendrerit, nisl arcu molestie arcu, at consectetur nisi diam eget dolor. Cras laoreet enim in metus
+consequat convallis. Proin ac neque mi. Suspendisse consequat nunc est, eget lacinia metus pellentesque ut. Maecenas id
+ipsum molestie, venenatis nunc in, congue purus. Quisque tincidunt mi semper pharetra sagittis. Ut viverra in mi
+malesuada placerat. Phasellus cursus id mi at sodales. In dolor magna, tristique eu bibendum at, posuere ut nisi.
+Integer nec elit quis augue dignissim lacinia at nec leo. Donec id dui congue, ullamcorper sapien id, commodo est.
+Aliquam ornare gravida nulla eget convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+cubilia curae; Nulla ut leo eget urna convallis euismod. Class aptent taciti sociosqu ad litora torquent per conubia
+nostra, per inceptos himenaeos. Donec non purus at dui rhoncus volutpat facilisis vitae urna. Donec a feugiat ex. Cras
+pulvinar luctus est, sit amet congue metus tempus nec. Suspendisse potenti. Praesent est metus, accumsan sit amet
+eleifend ut, blandit et erat. Suspendisse mi libero, auctor eget enim sodales, maximus vestibulum arcu. Aliquam suscipit
+placerat tempus. Donec pretium consequat pretium. Etiam ut orci vel est fermentum feugiat. Phasellus sed justo diam.
+Aliquam in purus egestas, sodales nisi at, ultrices quam. Proin sodales a nulla vitae pharetra. Sed hendrerit magna a
+ante mattis, et congue nisl dapibus. Donec in blandit nisi. Nunc sit amet risus sed felis rhoncus tincidunt eget sit
+amet elit. Etiam sit amet lacinia metus. Aenean mattis velit vitae maximus gravida. Proin dignissim dui sit amet diam
+ornare porta. Quisque eu ante non felis finibus laoreet sed at elit.
